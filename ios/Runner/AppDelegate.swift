0f7d4c8e33812229bf337d12b4f22a69f6401def
daniel <fusendaniel20020115@gmail.com>
2021-12-01 17:10:14 +0900

    ローカル通知の処理作成

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -5,9 +5,16 @@ import Flutter
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    didFinishLaunchingWithOptions launchOptios: [UIApplication.LaunchOptions Key: Any]?
   ) -> Bool {
+
+     // flutter_local_notification
+     if #available(iOS 10.0, *) {
+       UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+     }
+
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    }
 }
